ee4789290c30d8afc14275d3c79b145b123d94b1
oldmonk7 <shashank@travelbank.com>
2018-06-29 23:08:57 -0700

    Channel and messages endpoint

--- server.js
+++ server.js
@@ -4,10 +4,41 @@ var io = require('socket.io')(http);
 // var db = require('./db');
 var Room = require('./models/room');
 var Person = require('./models/person');
+const NUM_OF_MSG_PER_REQUEST = 3;
 
 app.get('/', function(req, res){
   res.send({ hello: "world"});
 });
+
+app.get('/Channel', (req, res) => {
+	const url = req.query.url;
+	Room.findOneAndUpdate({ url }, {}, { upsert: true, new: true }, function (err, room) {
+		res.send({ id: room._id });
+	});
+});
+
+app.get('/Channel/:id/messages', (req, res) => {
+	const channelId = req.params.id;
+	const page = req.query.page || 1;
+	if (!channelId) {
+		throw new Error('No channel Id in the request');
+	}
+	Room.findOne({ _id: channelId }, (err, room) => {
+		if (!room || !room.messages) {
+			res.send([]);
+		}
+		const messages = room.messages;
+		const lastNItems = messages.slice(-1 * Math.min(NUM_OF_MSG_PER_REQUEST * page, messages.length));
+		let result = [];
+		if (lastNItems.length === messages.length) {
+			result = messages.slice(0, messages.length % NUM_OF_MSG_PER_REQUEST);
+		} else {
+			console.log(lastNItems);
+			result = lastNItems.slice(0, NUM_OF_MSG_PER_REQUEST);
+		}
+		res.send({ messages: result });
+	})
+})
 
 
 var data = [];
